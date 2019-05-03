95a0f5685ce6b4b411d6783df3737b455e8bf631
Adrian Oboroc <oboroc@users.noreply.github.com>
2019-05-03 16:21:46 -0400

    use string as input for flex scanner

--- src/crass.c
+++ src/crass.c
@@ -4,9 +4,26 @@
 
 #include <stdio.h>
 #include <assert.h>
+#include <string.h>
 
 extern FILE* yyin, * yyout;
 extern int yylex(void);
+
+static size_t read_pos;
+static const char* input_buf = "123 abc";
+int lexer_input(char* buf, int* result, int max_size)
+{
+	size_t total_char = max_size;
+	size_t char_to_read = strlen(input_buf) - read_pos;
+	int i;
+	if (total_char > char_to_read)
+		total_char = char_to_read;
+	for (i = 0; i < total_char; i++)
+		buf[i] = input_buf[read_pos + i];
+	*result = total_char;
+	read_pos += total_char;
+	return 0;
+}
 
 int yywrap(void)
 {
@@ -15,18 +32,5 @@ int yywrap(void)
 
 int main(int argc, char** argv)
 {
-	errno_t err;
-
-	/* skip over program name */
-	argv++;
-	argc--;
-	if (argc > 0)
-	{
-		err = fopen_s(&yyin, argv[0], "r");
-		assert(err == 0);
-	}
-	else
-		yyin = stdin;
-
 	yylex();
 }
