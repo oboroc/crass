6d28bd38527131b2450f460965679c41287459bf
Adrian Oboroc <oboroc@users.noreply.github.com>
2019-05-03 22:19:25 -0400

    switch from W3 to W4 warning level

--- src/crass.c
+++ src/crass.c
@@ -8,20 +8,32 @@
 
 #include "scanner.flex.h"
 
+#define UNUSED(x) (void)(x)
+
 int yywrap(yyscan_t yyscanner)
 {
+	UNUSED(yyscanner);
 	return 1;
 }
 
-int main(int argc, char** argv)
+void scan_str(char *str)
 {
 	/* use reentrant scanner */
 	yyscan_t scanner;
 	YY_BUFFER_STATE buf;
 	yylex_init(&scanner);
-	buf = yy_scan_string("0abch 01234h 0ffffh", scanner);
+	buf = yy_scan_string(str, scanner);
 	yylex(scanner);
 	yy_delete_buffer(buf, scanner);
 	yylex_destroy(scanner);
+}
+
+int main(int argc, char** argv)
+{
+	UNUSED(argc);
+	UNUSED(argv);
+	scan_str("0abch 01234h 0ffffh");
+	scan_str("A B C");
+	scan_str("0.1 .2 1.0003");
 	return 0;
 }
